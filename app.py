9030985c85fa40ba7b7b7031b1f15bbdedcd864e
noamaanmasood <noamaanmasood2005@gmail.com>
2026-05-10 19:05:48 +0530

    Ready for Streamlit deployment

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ st.set_page_config(
 # -----------------------------
 # LOAD DATA
 # -----------------------------
-df = pd.read_csv("StockX-Data-Contest-2019-3.csv")
+df = pd.read_csv("data/StockX-Data-Contest-2019-3.csv")
 
 # -----------------------------
 # TITLE
